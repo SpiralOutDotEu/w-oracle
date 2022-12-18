3c89844a67b26d9b837695602db36aa1bc43f3ea
Jumaru <spiralout.eu@gmail.com>
2022-12-18 17:10:34 +0200

    owner can add oracles

--- contracts/WeatherOracle.sol
+++ contracts/WeatherOracle.sol
@@ -4,6 +4,16 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract WeatherOracle is Ownable {
+  mapping(address => bool) oracles;
   constructor() public {
+  }
+
+  function addOracle(address _oracleAddress) public onlyOwner{
+    oracles[_oracleAddress] = true;
+  }
+
+  function isOracle(address _oracleAddress) public view returns(bool){
+    bool _isOracle = oracles[_oracleAddress];
+    return _isOracle;
   }
 }
--- test/weather_oracle.js
+++ test/weather_oracle.js
@@ -1,12 +1,13 @@
 const WeatherOracle = artifacts.require("WeatherOracle");
 const { expect } = require('chai');
 
+
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("WeatherOracle", function ([owner, oracle1]) {
+contract("WeatherOracle", function ([owner, oracle1, oracle2, other]) {
   beforeEach(async function () {
     this.oracle = await WeatherOracle.new({from: owner})
   })
@@ -20,4 +21,16 @@ contract("WeatherOracle", function ([owner, oracle1]) {
     const _owner = await this.oracle.owner.call()
     expect(_owner).to.equal(owner);
   })
+
+  it("owner can add oracles", async function(){
+    await this.oracle.addOracle(oracle1, {from: owner});
+    await this.oracle.addOracle(oracle2, {from: owner});
+    const _isOracle1Whitelisted = await this.oracle.isOracle.call(oracle1);
+    const _isOracle2Whitelisted = await this.oracle.isOracle.call(oracle2);
+    const _isOtherWhitelisted = await this.oracle.isOracle.call(other);
+    expect(_isOracle1Whitelisted).to.equal(true);
+    expect(_isOracle2Whitelisted).to.equal(true);
+    expect(_isOtherWhitelisted).to.equal(false);
+  })
+
 });
